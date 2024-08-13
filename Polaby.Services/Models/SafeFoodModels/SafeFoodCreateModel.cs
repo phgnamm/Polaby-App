266059f2ca9d02417212ef3717eb7f2d642ded2e
TinhCao <trancao134@gmail.com>
2024-08-13 20:57:38 +0700

    feat: safefood

---
+++ Polaby.Services/Models/SafeFoodModels/SafeFoodCreateModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Polaby.Services.Models.SafeFoodModels;
+public class SafeFoodCreateModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+
+    [Required(ErrorMessage = "IsSafe status is required")]
+    public bool IsSafe { get; set; }
+    public string? Source { get; set; }
+    public string? SourceUrl { get; set; }
+}
