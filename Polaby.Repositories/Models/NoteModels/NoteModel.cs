476771545f0da8c6bb78c892be74ebbb5e2c956f
phgnam <namdpse170517@fpt.edu.vn>
2024-08-14 22:13:57 +0700

    fix: update code Note

--- Polaby.Repositories/Models/NoteModels/NoteModel.cs
+++ Polaby.Repositories/Models/NoteModels/NoteModel.cs
@@ -1,4 +1,5 @@
-﻿using Polaby.Repositories.Enums;
+﻿using Polaby.Repositories.Entities;
+using Polaby.Repositories.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Polaby.Repositories.Models.NoteModels
 {
-    public class NoteModel
+    public class NoteModel : BaseEntity
     {
         public string? Title { get; set; }
         public Trimester Trimester { get; set; }
