2cec35e215aab8f8c263330c9f12dfdb9f6a0917
Dang Van Hai <vanhaicntt5@gmail.com>
2024-08-25 22:20:22 +0700

    fix: update account

--- Polaby.Services/Models/AccountModels/AccountFilterModel.cs
+++ Polaby.Services/Models/AccountModels/AccountFilterModel.cs
@@ -12,6 +12,13 @@ public class AccountFilterModel: PaginationParameter
     public bool IsDeleted { get; set; } = false;
     public Gender? Gender { get; set; }
     public string? Search { get; set; }
+    public bool? IsSubscriptionActive { get; set; }
+    public bool? EmailConfirmed { get; set; }
+    public BMI? BMI { get; set; }
+    public Diet? Diet { get; set; }
+    public FrequencyOfActivity? FrequencyOfActivity { get; set; }
+    public FrequencyOfStress? FrequencyOfStress { get; set; }
+    public Level? Level { get; set; }
     // protected override int MinPageSize { get; set; } = PaginationConstant.ACCOUNT_MIN_PAGE_SIZE;
     // protected override int MaxPageSize { get; set; } = PaginationConstant.ACCOUNT_MAX_PAGE_SIZE;
 }
--- Polaby.Services/Models/AccountModels/Validation/DueDateValidation.cs
+++ Polaby.Services/Models/AccountModels/Validation/DueDateValidation.cs
@@ -6,15 +6,21 @@ public class DueDateValidation : ValidationAttribute
 {
     public override bool IsValid(object? value)
     {
+        if (value == null)
+        {
+            return true;
+        }
+        
         if (value is not DateOnly dueDate)
         {
             return false;
         }
 
-        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+        var today = DateOnly.FromDateTime(DateTime.Now);
         var maxDueDate = today.AddDays(42 * 7); // 42 weeks * 7 days per week
 
-        return dueDate <= maxDueDate || dueDate <= today;
+        // return dueDate >= maxDueDate || dueDate <= today;
+        return dueDate < maxDueDate && dueDate > today;
     }
 
     public override string FormatErrorMessage(string name)
