d606ae85f50ea5496b8c065e7c7eefed1c99df29
lc-tn <lamtanloc3@gmail.com>
2024-08-26 20:44:07 +0700

    feat: check password

--- Polaby.API/Hubs/NotificationHub.cs
+++ Polaby.API/Hubs/NotificationHub.cs
@@ -4,24 +4,24 @@ namespace Polaby.API.Hubs
 {
     public class NotificationHub : Hub
     {
-        private readonly ILogger<NotificationHub> _logger;
+        //private readonly ILogger<NotificationHub> _logger;
 
-        public NotificationHub(ILogger<NotificationHub> logger)
-        {
-            _logger = logger;
-        }
-
-        //public override async Task OnConnectedAsync(string message)
+        //public NotificationHub(ILogger<NotificationHub> logger)
         //{
-        //    //_logger.LogInformation("Client connected: {ConnectionId}", Context.ConnectionId);
-        //    await Clients.All.SendAsync("ReceiveMessage", message);
-        //    //await Clients.All.SendAsync("ReceiveNotification", message);
+        //    _logger = logger;
         //}
 
-        public async Task SendNotification(string message)
+        public override async Task OnConnectedAsync()
         {
-            await Clients.All.SendAsync("ReceiveNotification", message);
+            //_logger.LogInformation("Client connected: {ConnectionId}", Context.ConnectionId);
+            //await Clients.All.SendAsync("ReceiveMessage", message);
+            await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} has connected");
         }
+
+        //public async Task SendNotification(string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveNotification", message);
+        //}
         //public async Task SendNotification(string message)
         //{
         //    await Clients.All.SendAsync(message);
